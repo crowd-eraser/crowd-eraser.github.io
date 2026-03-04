efa26818e9519a4c9c5b2e56bfe6ad659ef016d1
Yujin Ham <116600073+yujinh22@users.noreply.github.com>
2026-03-04 16:12:21 -0600

    index.js

--- static/js/index.js
+++ static/js/index.js
@@ -39,7 +39,17 @@ $(document).ready(function() {
     }
 
 		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
+    // var carousels = bulmaCarousel.attach('.carousel', options);
+    var carousels = bulmaCarousel.attach('.carousel', {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      breakpoints: [
+        { changePoint: 480, slidesToShow: 1, slidesToScroll: 1 },
+        { changePoint: 640, slidesToShow: 1, slidesToScroll: 1 },
+        { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 },
+        { changePoint: 9999, slidesToShow: 1, slidesToScroll: 1 }
+      ]
+    });
 
     // Loop on each carousel initialized
     for(var i = 0; i < carousels.length; i++) {
